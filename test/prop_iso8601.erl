64b4bf66bf870aaeef4d9f42cc195de642880ea2
Duncan McGreggor <oubiwann@gmail.com>
2026-05-25 21:45:10 -0500

    Fixed formatting and linting.

--- test/prop_iso8601.erl
+++ test/prop_iso8601.erl
@@ -15,8 +15,20 @@ prop_parse_format_roundtrip() ->
 prop_interval_start_end_roundtrip() ->
     ?FORALL(
         {Y1, Mo1, D1, H1, Mn1, S1, Y2, Mo2, D2, H2, Mn2, S2},
-        {range(1, 9999), range(1, 12), range(1, 28), range(0, 23), range(0, 59), range(0, 59),
-         range(1, 9999), range(1, 12), range(1, 28), range(0, 23), range(0, 59), range(0, 59)},
+        {
+            range(1, 9999),
+            range(1, 12),
+            range(1, 28),
+            range(0, 23),
+            range(0, 59),
+            range(0, 59),
+            range(1, 9999),
+            range(1, 12),
+            range(1, 28),
+            range(0, 23),
+            range(0, 59),
+            range(0, 59)
+        },
         begin
             Start = {{Y1, Mo1, D1}, {H1, Mn1, S1}},
             End = {{Y2, Mo2, D2}, {H2, Mn2, S2}},
@@ -28,16 +40,38 @@ prop_interval_start_end_roundtrip() ->
 prop_start_duration_bounds() ->
     ?FORALL(
         {Y, Mo, D, H, Mn, S, DY, DM, DD, DH, DMi, DS},
-        {range(1, 5000), range(1, 12), range(1, 28), range(0, 23), range(0, 59), range(0, 59),
-         range(0, 10), range(0, 11), range(0, 28), range(0, 23), range(0, 59), range(0, 59)},
+        {
+            range(1, 5000),
+            range(1, 12),
+            range(1, 28),
+            range(0, 23),
+            range(0, 59),
+            range(0, 59),
+            range(0, 10),
+            range(0, 11),
+            range(0, 28),
+            range(0, 23),
+            range(0, 59),
+            range(0, 59)
+        },
         begin
             Start = {{Y, Mo, D}, {H, Mn, S}},
-            Dur = [{sign, ""}, {years, DY}, {months, DM}, {days, DD},
-                   {hours, DH}, {minutes, DMi}, {seconds, DS}],
+            Dur = [
+                {sign, ""},
+                {years, DY},
+                {months, DM},
+                {days, DD},
+                {hours, DH},
+                {minutes, DMi},
+                {seconds, DS}
+            ],
             I = {interval, start_duration, Start, Dur},
             {_, End} = iso8601:interval_bounds(I),
-            End =:= iso8601:apply_duration(Start,
-                binary_to_list(iolist_to_binary(format_dur(Dur))))
+            End =:=
+                iso8601:apply_duration(
+                    Start,
+                    binary_to_list(iolist_to_binary(format_dur(Dur)))
+                )
         end
     ).
 
@@ -48,26 +82,30 @@ format_dur(Plist) ->
     H = proplists:get_value(hours, Plist, 0),
     Mi = proplists:get_value(minutes, Plist, 0),
     S = proplists:get_value(seconds, Plist, 0),
-    DateParts = [integer_to_list(V) ++ U ||
-        {V, U} <- [{Y, "Y"}, {Mo, "M"}, {D, "D"}], V > 0],
-    TimeParts = [integer_to_list(V) ++ U ||
-        {V, U} <- [{H, "H"}, {Mi, "M"}, {S, "S"}], V > 0],
-    TimeSec = case TimeParts of
-        [] -> "";
-        _ -> "T" ++ lists:flatten(TimeParts)
-    end,
+    DateParts = [
+        integer_to_list(V) ++ U
+     || {V, U} <- [{Y, "Y"}, {Mo, "M"}, {D, "D"}], V > 0
+    ],
+    TimeParts = [
+        integer_to_list(V) ++ U
+     || {V, U} <- [{H, "H"}, {Mi, "M"}, {S, "S"}], V > 0
+    ],
+    TimeSec =
+        case TimeParts of
+            [] -> "";
+            _ -> "T" ++ lists:flatten(TimeParts)
+        end,
     lists:flatten(["P", lists:flatten(DateParts), TimeSec]).
 
 prop_expanded_roundtrip() ->
     ?FORALL(
         {Y, Mo, D, H, Mn, S},
-        {range(-9999, 9999), range(1, 12), range(1, 28),
-         range(0, 23), range(0, 59), range(0, 59)},
+        {range(-9999, 9999), range(1, 12), range(1, 28), range(0, 23), range(0, 59), range(0, 59)},
         begin
             DT = {{Y, Mo, D}, {H, Mn, S}},
             Formatted = binary_to_list(iso8601:format_expanded(DT)),
             {{PY, PMo, PD}, {PH, PMn, PS}} = iso8601:parse_expanded(Formatted),
             Y =:= PY andalso Mo =:= PMo andalso D =:= PD andalso
-            H =:= PH andalso Mn =:= PMn andalso S =:= round(PS)
+                H =:= PH andalso Mn =:= PMn andalso S =:= round(PS)
         end
     ).
